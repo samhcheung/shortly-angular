b5274ed32d93b66d477bd5ea0b773388cdf014d8
Drew Kosta <drewkosta@gmail.com>
2016-08-27 10:16:22 -0700

    stashing changes

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   errorHandler: function (error, req, res, next) {
     // send error message to client
-    // message for gracefull error handling on app
+    // message for graceful error handling on app
     res.status(500).send({error: error.message});
   },
 
